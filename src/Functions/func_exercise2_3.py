d9b238a4bb1316d7cdfa208fffa60673350dbb0f
RandyGuo <work_guomz@163.com>
2016-04-02 11:46:26 +0800

    Define a func get_doc(module),return the doc of the module

---
+++ src/Functions/func_exercise2_3.py
@@ -0,0 +1,9 @@
+'''
+Created on 2016年4月2日
+Problem 2_3: 定义一个方法  get_doc(module),module 参数为该脚本中导入或定义的模块对象，该函数返回module的帮助文档。
+example: print get_doc(urllib),则会输出urllib这个模块的帮助文档。
+@author: RandyGuo
+'''
+
+if __name__ == '__main__':
+    pass
