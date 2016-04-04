e7dcf44138618a57b765a1360ceb37f153d21c9e
RandyGuo <work_guomz@163.com>
2016-04-04 16:33:10 +0800

    class exercise 2

---
+++ CorePython/c1.py
@@ -0,0 +1,10 @@
+class test(object):
+    """
+    get 被称之为test对象的方法
+    """
+
+    def get(self,a):
+        return "From class function get(): Heihei"
+
+def get():
+    return "from outer function"
